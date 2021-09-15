072768d48beb60cf8d058c4a8975fc6ccc7cc073
Sanat Kumar Dubey <studentia312@gmail.com>
2021-09-15 21:51:39 +0530

    final commit

--- src/watermelon.java
+++ src/watermelon.java
@@ -4,7 +4,7 @@ public class watermelon {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int w= sc.nextInt();
-        if(w%2==0){
+        if(w!=2 && w%2==0){
             System.out.println("YES");
         }
         else{
