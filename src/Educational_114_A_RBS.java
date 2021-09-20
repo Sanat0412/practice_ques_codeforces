3e3c99215377597529313578be47644ef95c6155
Sanat Kumar Dubey <studentia312@gmail.com>
2021-09-20 20:27:53 +0530

    first and final commit

---
+++ src/Educational_114_A_RBS.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Educational_114_A_RBS {
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            ArrayList<String> jhaat = new ArrayList<String>();
+            geppppp(n, 0, 0, "", jhaat);
+            for (int i = 0; i < n; i++) {
+                System.out.println(jhaat.get(i));
+            }
+
+
+        }
+    }
+
+    static void geppppp(int n, int khula, int band, String s, ArrayList<String> gaand) {
+
+        if (gaand.size() > n) {
+            return;
+        }
+
+        if (khula == n && band == n) {
+            gaand.add(s);
+            return;
+        }
+
+        if (khula < n) {
+            geppppp(n, khula + 1, band, s + "(", gaand);
+        }
+
+        if (band < khula) {
+            geppppp(n, khula, band + 1, s + ")", gaand);
+        }
+    }
+
+}
